2f48451d9785d55e75313a48e88defb35d3780e4
NicoP93 <nicopcrr@gmail.com>
2019-03-12 17:46:09 -0500

    Trying to get barebones functionality here

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,25 @@
+$("document").ready(function () {
+
+  var userCorrect = 0;
 
 
-    
+
+  function check() {
+    if ($('input[gender1]:checked').val() = Male) {
+      userCorrect++
+    }
+    if ($('input[gender2]:checked').val() = Female) {
+      userCorrect++
+    }
+    if ($('input[gender3]:checked').val() = Other) {
+      userCorrect++
+    }
+
+    $("#howmanycorrect").text(userCorrect);
+  }
+
+  
+  $("#submit").click(check());
+
+
+});
